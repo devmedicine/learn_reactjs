841b6aaf77904f4e782f861cd60991bb1301801e
devmedicine <daikiti412@gmail.com>
2019-01-09 23:13:34 +0900

    マスクリックで対象マスにX文字列を描画するように変更。

--- src/index.js
+++ src/index.js
@@ -3,42 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square" onClick={() => alert('click') }>
-                {this.props.value}
-            </button>
+          <button
+            className="square"
+            onClick={() => this.setState({value: 'X'})}
+          >
+              {this.state.value}
+          </button>
         );
     }
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={i}/>;
     }
 
     render() {
         const status = 'Next player: X';
 
         return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+          <div>
+              <div className="status">{status}</div>
+              <div className="board-row">
+                  {this.renderSquare(0)}
+                  {this.renderSquare(1)}
+                  {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+              </div>
+          </div>
         );
     }
 }
@@ -46,15 +55,15 @@ class Board extends React.Component {
 class Game extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
+          <div className="game">
+              <div className="game-board">
+                  <Board/>
+              </div>
+              <div className="game-info">
+                  <div>{/* status */}</div>
+                  <ol>{/* TODO */}</ol>
+              </div>
+          </div>
         );
     }
 }
@@ -62,6 +71,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  <Game/>,
+  document.getElementById('root')
 );
